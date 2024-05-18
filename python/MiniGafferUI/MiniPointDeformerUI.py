6674b144cf2a89ef841dbcaee7ef386fe7bdb594
tengu <tengu.mini@gmail.com>
2024-05-17 18:12:03 -0700

    MiniGafferUI: PointDeformer - Update plug descriptions.

--- python/MiniGafferUI/MiniPointDeformerUI.py
+++ python/MiniGafferUI/MiniPointDeformerUI.py
@@ -18,20 +18,18 @@ Gaffer.Metadata.registerNode(
             "Path to location which contains a primitive who's points the filtered geometry will be deformed by"
         ],
         "WeightsPrimVar": [
-            "description", ""
+            "description",
+            "Name of the constant float vector primvar (size = num points in render mesh * maxWeights) which contains the weight of the influence of the cage primitive vertex referenced the matching value in IndicesPrimVar "
         ],
         "IndicesPrimVar": [
-            "description", ""
+            "description",
+            "Name of a the constant int vector primvar (size = num points in render mesh * maxWeights) which contains the index to the point in the cage mesh which should contribute to the deformation"
         ],
         "BindTransform": [
-            "description", ""
+            "description",
+            "Name of the Vertex Matrix44 attribute which transforms from the reference space to animated space."
         ]
-    },
+    }
 
 )
 
-#
-# addChild( new StringPlug( "cageLocation", Plug::In, "/cage" ) );
-# addChild( new StringPlug( "WeightsPrimVar", Plug::In, "PointBindWeights" ) );
-# addChild( new StringPlug( "IndicesPrimVar", Plug::In, "PointBindIndices" ) );
-# addChild( new StringPlug( "BindTransform", Plug::In, "BindTransform" ) );
